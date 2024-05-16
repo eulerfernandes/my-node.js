c70b1b3e10a49864d673735e02fac12871d34350
eulerfernandes <pedraeuler@gmail.com>
2024-05-16 20:55:42 -0300

    Initialize project using Create React App

---
+++ src/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+`;
+
+export const Image = styled.div`
+`;
+
+export const ContainerItens = styled.div`
+`;
+
+
+export const H1 = styled.h1`
+`;
+
+export const InputLabel = styled.p`
+`;
+
+export const Input = styled.input`
+`;
+
+export const Button = styled.button`
+`;
