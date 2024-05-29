acf9262ffb9950742abf94944c69172b3bfd1aff
eulerfernandes <pedraeuler@gmail.com>
2024-05-28 21:08:46 -0300

    adding style.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Container, Img, ContainerItens, H1, InputLabel, Input, Button } from "./style";
+
+function App(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image />
+      <ContainerItens>
+        <H1>Olá!</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input placeholder="Nome" />
+
+        <InputLabel>Idades</InputLabel>
+        <Input placeholder="Idade" />
+
+        <Button>Cadastrar</Button>
+        
+        </ContainerItens>
+      
+    </Container>
   );
 }
 
